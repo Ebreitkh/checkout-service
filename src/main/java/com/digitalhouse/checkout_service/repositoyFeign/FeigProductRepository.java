7235b87ecfdb9390e04857c2f6c7aa648b6c5a0f
Ebreitkh <ebreitkh@gmail.com>
2025-04-08 07:35:21 -0500

    se cambia el algoritmo Round Robin,para poder enviar a diferentes instancias, no hay un patron es al azar.

--- src/main/java/com/digitalhouse/checkout_service/repositoyFeign/FeigProductRepository.java
+++ src/main/java/com/digitalhouse/checkout_service/repositoyFeign/FeigProductRepository.java
@@ -1,16 +1,15 @@
 package com.digitalhouse.checkout_service.repositoyFeign;
 
 
-import com.digitalhouse.checkout_service.configuration.LoadBalancerConfiguration;
+
 import com.digitalhouse.checkout_service.model.dto.Product;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "products-service")
-@LoadBalancerClient(value = "products-service", configuration = LoadBalancerConfiguration.class)
+
 public interface FeigProductRepository {
 
     @RequestMapping(method = RequestMethod.GET, value = "/products")
